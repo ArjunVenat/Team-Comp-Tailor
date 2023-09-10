0e855d5c8c71e8af2f383077a22c777de9f37cdb
ArjunVenat03 <arjunvenat@gmail.com>
2023-09-10 00:17:32 +0000

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    ENDPOINT1: process.env.ENDPOINT1,
+    ENDPOINT2: process.env.ENDPOINT2,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,217 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useEffect, useState } from "react";
+import { Button, Autocomplete, TextField, Checkbox, Stack, Typography } from "@mui/material";
+import { AutocompleteChangeReason, AutocompleteChangeDetails } from "@mui/material/Autocomplete";
 
-const inter = Inter({ subsets: ['latin'] })
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import SendIcon from "@mui/icons-material/Send";
 
-export default function Home() {
+
+function index() {
+  
+  //Endpoint1 is the http request for when the button is clicked
+  //Endpoint2 is the http request for the autocomplete options when the site loads
+  const apiEndpoint1 = process.env.ENDPOINT1 as string
+  const apiEndpoint2 = process.env.ENDPOINT2 as string
+
+  //Checkbox Autocomplete settings
+  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+  const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+  //Options for Checkbox Autocompletes
+  const [champKeys, setChampKeys] = useState<string[]>([]);
+  const [traitKeys, setTraitKeys] = useState<string[]>([]);
+
+  //Overriding Button Styles
+  const [isHovered, setIsHovered] = useState(false);
+  const buttonStyle = {
+    backgroundColor: isHovered ? "#1565c0" : "#1976d2",
+  };
+
+  //User selected options
+  const [selectedIncludedChamps, setSelectedIncludedChamps] = useState<string[]>([]);
+  const [selectedExcludedChamps, setSelectedExcludedChamps] = useState<string[]>([]);
+  const [selectedPriorityTraits, setSelectedPriorityTraits] = useState<string[]>([]);
+
+  //Output information
+  const [optimalBoard, setOptimalBoard] = useState("____");
+  const [traitInfo, setTraitInfo] = useState("____");
+  const [traitSynergyScore, settraitSynergyScore] = useState("____");
+
+  const handleIncludedChampsChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    value: readonly string[],
+    reason: AutocompleteChangeReason,
+    details?: AutocompleteChangeDetails<string> | undefined
+  ) => {
+    setSelectedIncludedChamps(value as string[]);
+  };
+
+  const handleExcludedChampsChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    value: readonly string[],
+    reason: AutocompleteChangeReason,
+    details?: AutocompleteChangeDetails<string> | undefined
+  ) => {
+    setSelectedExcludedChamps(value as string[]);
+  };
+
+  const handlePriorityChampsChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    value: readonly string[],
+    reason: AutocompleteChangeReason,
+    details?: AutocompleteChangeDetails<string> | undefined
+  ) => {
+    setSelectedPriorityTraits(value as string[]);
+  };
+
+  //Button Click
+  const handleSubmission = async () => {
+    const data = {
+      includedChamps: selectedIncludedChamps,
+      excludedChamps: selectedExcludedChamps,
+      priorityTraits: selectedPriorityTraits,
+    };
+
+    try {
+        const response = await fetch(apiEndpoint1 , {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+      if (response.ok) {
+        const responseData = await response.json();
+
+        const optimalBoard = responseData[0].join(", ");
+        const traitInfo = responseData[1].join(", ");
+        const traitSynergyScore = responseData[2];
+
+        setOptimalBoard(optimalBoard);
+        setTraitInfo(traitInfo);
+        settraitSynergyScore(traitSynergyScore);
+      } 
+      else {
+        // Handle error response
+      }
+    } 
+    catch (error) {
+        console.error("Error:", error);
+    }
+  };
+
+  //Checkbox Options
+  useEffect(() => {
+    fetch(apiEndpoint2)
+      .then((response) => response.json())
+      .then((data) => {
+        const champKeys = Object.keys(data.champs);
+        const traitKeys = Object.keys(data.traits);
+
+        setChampKeys(champKeys);
+        setTraitKeys(traitKeys);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="index">
+      <Stack spacing={8} alignItems="center" justifyContent="center">
+        <Stack spacing={4} alignItems="center" justifyContent="center">
+        <Typography variant="h5"><b>Select Model Parameters</b></Typography>
+          <Autocomplete
+            multiple
+            id="included_champs_selector"
+            options={champKeys}
+            disableCloseOnSelect
+            value={selectedIncludedChamps}
+            onChange={handleIncludedChampsChange}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option}
+              </li>
+            )}
+            style={{ width: 500 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Included Champions" />
+            )}
+          />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <Autocomplete
+            multiple
+            id="excluded_champs_selector"
+            options={champKeys}
+            disableCloseOnSelect
+            value={selectedExcludedChamps}
+            onChange={handleExcludedChampsChange}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option}
+              </li>
+            )}
+            style={{ width: 500 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Excluded Champions" />
+            )}
+          />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Autocomplete
+            multiple
+            id="priority_traits_selector"
+            options={traitKeys}
+            disableCloseOnSelect
+            value={selectedPriorityTraits}
+            onChange={handlePriorityChampsChange}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option}
+              </li>
+            )}
+            style={{ width: 500 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Priority Traits" />
+            )}
+          />
+          <Button
+          style={buttonStyle}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          variant="contained"
+          onClick={handleSubmission}
+          endIcon={<SendIcon />}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Submit Choices
+        </Button>
+        </Stack>
+        
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <Typography><b>Selected Champions:</b> {optimalBoard}</Typography>
+        <Typography><b>Active Traits: </b>{traitInfo} <b> with a total synergy score of </b> {traitSynergyScore}</Typography>
+      </Stack>
+    </div>
+  );
 }
+
+export default index;
